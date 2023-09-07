ee10dffe9c8f278fa529de039a1013fac8939526
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-09-07 07:51:31 +0300

    Add the readme file and create database and tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
+CREATE DATABASE Vet_Clinic;
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+ CREATE TABLE animals ( 
+    id int, 
+    name VARCHAR(100), 
+    date_of_birth date, 
+    escape_attempts int, 
+    neutered boolean, 
+    weight_kg decimal
+    );
+    
